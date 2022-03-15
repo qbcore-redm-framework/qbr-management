2cd6cb2a9cc65abe2efcde9774711c086b29f503
Polar <altpolar0@gmail.com>
2022-03-15 23:05:43 +0200

    main push

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.Key = 0xCEFD9220 --E
+
+Config.Jobs = {
+    ['police'] = vector3(-328.8274, 784.4441, 116.3731),
+    ['ambulance'] = vector3(310.1, -599.43, 43.29),
+    ['realestate'] = vector3(-124.786, -641.486, 168.820),
+    ['taxi'] = vector3(903.32, -170.55, 74.0),
+    ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
+    ['mechanic'] = vector3(-347.41, -133.48, 39.01)
+}
+
+Config.Gangs = {
+    ['lostmc'] = vector3(0, 0, 0),
+    ['ballas'] = vector3(0,0, 0),
+    ['vagos'] = vector3(0, 0, 0),
+    ['cartel'] = vector3(0, 0, 0),
+    ['families'] = vector3(0, 0, 0)
+}
